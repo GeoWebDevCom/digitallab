81f78b207494cf89acb329df0066557db785a9e2
fglukhov <f.glukhov@gmail.com>
2014-08-12 16:44:53 +0400

    Ver. 1.6

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -4,6 +4,54 @@
 
 $(document).ready(function() {
 
+	// Grayscale IE 10-11
+
+	var isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
+
+	if (/*@cc_on!@*/false || isIE11) {
+		$('.portfolio-item img').each(function(){
+			var el = $(this);
+			el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"5","opacity":"0"}).insertBefore(el).queue(function(){
+				var el = $(this);
+				el.parent().css({"width":this.width,"height":this.height});
+				el.dequeue();
+			});
+			this.src = grayscaleIE10(this.src);
+		});
+		
+		// Quick animation on IE10+ 
+		$('img').hover(
+			function () {
+				$(this).parent().find('img:first').stop().animate({opacity:1}, 200);
+			}, 
+			function () {
+				$('.img_grayscale').stop().animate({opacity:0}, 200);
+			}
+		);	
+		
+		function grayscaleIE10(src){
+			var canvas = document.createElement('canvas');
+			var ctx = canvas.getContext('2d');
+			var imgObj = new Image();
+			imgObj.src = src;
+			canvas.width = imgObj.width;
+			canvas.height = imgObj.height; 
+			ctx.drawImage(imgObj, 0, 0); 
+			var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			for(var y = 0; y < imgPixels.height; y++){
+				for(var x = 0; x < imgPixels.width; x++){
+					var i = (y * 4) * imgPixels.width + x * 4;
+					var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+					imgPixels.data[i] = avg; 
+					imgPixels.data[i + 1] = avg; 
+					imgPixels.data[i + 2] = avg;
+				}
+			}
+			ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+			return canvas.toDataURL();
+		};
+	}
+
 	// Forms
 	
 	$("input:text").each(function() {
